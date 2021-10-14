19fd8d562618313bdf75c892552204d9ecdf4a9b
noursammour <nour.sammour@hotmail.com>
2021-10-15 00:17:10 +0300

    change docs path

--- app/ccp-bridge/src/main.ts
+++ app/ccp-bridge/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bodyParser: false });
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false,
+    logger: ['log', 'debug', 'error', 'verbose', 'warn'],
+  });
   await app.listen(3000);
 }
 bootstrap();
