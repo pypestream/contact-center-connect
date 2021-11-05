bb29bc961280b09df9dd97aaa96aacc5aa813128
noursammour <nour.sammour@hotmail.com>
2021-11-05 16:24:24 +0300

    remove useless files

--- lib/nestjs-ccp/example/src/AppService.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import Ccp from 'ccp';
-import { InjectCcp } from './../../src';
-
-@Injectable()
-export class AppService {
-  public constructor(@InjectCcp() private readonly ccpClient: Ccp) {}
-}
