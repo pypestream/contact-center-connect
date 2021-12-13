7f3c9586e8d99191e996b0a4c1211aacd467cd10
noursammour <nour.sammour@hotmail.com>
2021-12-13 17:02:56 +0300

    singleton websocket service

--- src/contact-centers/src/genesys/genesys.websocket.ts
+++ src/contact-centers/src/genesys/genesys.websocket.ts
@@ -1,44 +1,45 @@
-import { Inject, Injectable, Scope } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as WebSocket from 'ws';
 import { timer } from 'rxjs';
 import axios from 'axios';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
 import { differenceInMilliseconds, parseISO } from 'date-fns';
 import { GenesysWsConfig } from './types/genesys-ws-config';
-import { GenesysCustomer } from './types';
-import { getCustomer } from '../common/utils/get-customer';
+import { WebsocketConnection } from './types';
 import { MiddlewareApiService } from '../middleware-api/middleware-api.service';
 
 @Injectable()
 export class GenesysWebsocket {
   // wss://echo.websocket.org is a test websocket server
-  private ws: WebSocket[];
-  private isConnect: boolean[] = [];
 
-  private connections: GenesysWsConfig[] = [];
+  private connections: WebsocketConnection[] = [];
 
   constructor(private readonly middlewareApiService: MiddlewareApiService) {}
 
-  async addConnection(config: GenesysWsConfig) {
+  async addConnection(customer: GenesysWsConfig) {
     const connectionIndex = this.connections.findIndex(
-      (c) => c.clientId === config.clientId,
+      (c) => c.customer.clientId === customer.clientId,
     );
     if (connectionIndex === -1) {
-      this.connections.push(config);
-      await this.setupConnection(config, this.connections.length - 1);
+      const connection = {
+        customer,
+        isConnect: false,
+        ws: null,
+      };
+      this.connections.push(connection);
+      await this.setupConnection(this.connections.length - 1);
     }
   }
 
-  async setupConnection(config: GenesysWsConfig, connectionIndex) {
+  async setupConnection(connectionIndex: number) {
     const params = new URLSearchParams();
-    params.append('grant_type', config.grantType);
-    params.append('client_id', config.clientId);
-    params.append('client_secret', config.clientSecret);
-    const token = await axios.post(config.getTokenUrl, params);
+    const { customer } = this.connections[connectionIndex];
+    params.append('grant_type', customer.grantType);
+    params.append('client_id', customer.clientId);
+    params.append('client_secret', customer.clientSecret);
+    const token = await axios.post(customer.getTokenUrl, params);
     const { access_token, token_type } = token.data;
     const channel = await axios.post(
-      config.getChannelUrl,
+      customer.getChannelUrl,
       {},
       {
         headers: {
@@ -50,8 +51,8 @@ export class GenesysWebsocket {
     this.connect(connectUri, connectionIndex);
     await this.subscribeToChannel(
       id,
-      config.getChannelUrl,
-      config.queueId,
+      customer.getChannelUrl,
+      customer.queueId,
       token_type,
       access_token,
     );
@@ -61,43 +62,46 @@ export class GenesysWebsocket {
     );
     setTimeout(async () => {
       // connectUri expired close connection and re-connect
-      this.ws[connectionIndex].close();
-      await this.setupConnection(config, connectionIndex);
+      this.connections[connectionIndex].ws.close();
+      await this.setupConnection(connectionIndex);
     }, expireInMilliseconds);
   }
 
   connect(url: string, connectionIndex: number) {
-    this.ws[connectionIndex] = new WebSocket(url);
-    this.ws[connectionIndex].on('open', () => {
-      this.isConnect[connectionIndex] = true;
+    this.connections[connectionIndex].ws = new WebSocket(url);
+    this.connections[connectionIndex].ws.on('open', () => {
+      this.connections[connectionIndex].isConnect = true;
     });
 
-    this.ws[connectionIndex].on('error', (message) => {
-      this.ws[connectionIndex].close();
-      this.isConnect[connectionIndex] = false;
+    this.connections[connectionIndex].ws.on('error', (message) => {
+      this.connections[connectionIndex].ws.close();
+      this.connections[connectionIndex].isConnect = false;
     });
 
-    this.ws[connectionIndex].on('close', (message) => {
+    this.connections[connectionIndex].ws.on('close', (message) => {
       timer(1000).subscribe(() => {
-        this.isConnect[connectionIndex] = false;
+        this.connections[connectionIndex].isConnect = false;
         this.connect(url, connectionIndex);
       });
     });
 
-    this.ws[connectionIndex].on('message', async (stringifyMessage) => {
-      // HANDLER: your logic should goes here
-      // eslint-disable-next-line
-      const message = JSON.parse(stringifyMessage);
-      if (message.eventBody.participants) {
-        const conversationId =
-          message.eventBody.participants[0].attributes.conversationId;
-        const participant = message.eventBody.participants.pop();
-        if (this.isAgentDisconnected(participant)) {
-          //console.log('End chat conversation ID: ', conversationId);
-          await this.middlewareApiService.endConversation(conversationId);
+    this.connections[connectionIndex].ws.on(
+      'message',
+      async (stringifyMessage) => {
+        // HANDLER: your logic should goes here
+        // eslint-disable-next-line
+        const message = JSON.parse(stringifyMessage);
+        if (message.eventBody.participants) {
+          const conversationId =
+            message.eventBody.participants[0].attributes.conversationId;
+          const participant = message.eventBody.participants.pop();
+          if (this.isAgentDisconnected(participant)) {
+            //console.log('End chat conversation ID: ', conversationId);
+            await this.middlewareApiService.endConversation(conversationId);
+          }
         }
-      }
-    });
+      },
+    );
   }
 
   async subscribeToChannel(
--- src/contact-centers/src/genesys/types/index.ts
+++ src/contact-centers/src/genesys/types/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export { GenesysCustomer } from './genesys-customer';
 export { GenesysWsConfig } from './genesys-ws-config';
+export { WebsocketConnection } from './websocket-connection';
