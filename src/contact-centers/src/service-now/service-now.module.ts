54eca96bd2d3a3663bdf27ccaca3a17527ef6356
noursammour <nour.sammour@hotmail.com>
2022-05-24 14:24:49 +0300

    PE-20890: add FF

--- src/contact-centers/src/service-now/service-now.module.ts
+++ src/contact-centers/src/service-now/service-now.module.ts
@@ -6,9 +6,14 @@ import { ServiceNowService } from './service-now.service';
 import { MiddlewareApiModule } from '../middleware-api/middleware-api.module';
 import { forwardRef } from '@nestjs/common';
 import { HttpModule } from '../common/interceptors/http.module';
+import { FeatureFlagModule } from '../feature-flag/feature-flag.module';
 
 @Module({
-  imports: [forwardRef(() => MiddlewareApiModule), HttpModule],
+  imports: [
+    forwardRef(() => MiddlewareApiModule),
+    HttpModule,
+    FeatureFlagModule,
+  ],
   providers: [ServiceNowService],
   controllers: [ServiceNowController],
   exports: [ServiceNowService],
